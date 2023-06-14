c36fff4825a0c929727ff8225b333eda0afe7f32
Kuljit <kuljit.singh4444@gmail.com>
2023-06-15 01:34:52 +0530

    examples updated

--- src/helpers/apiConfig.js
+++ src/helpers/apiConfig.js
@@ -1,6 +1,8 @@
+const API_SAMPLE_RESPONSE = {"comments":[{"updatedBy":"Helen Stevens","comment":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam","updatedOn":"2022-08-11T09:18:10.123Z","taggedTo":[]},{"updatedBy":"Ethel Howard Daniel","comment":"Lorem ipsum dolor sit amet, consectetur adipiscing elit","updatedOn":"2022-08-11T08:30:46.235Z","taggedTo":["Dan Jenkin","John Doe"]},{"updatedBy":"Ethel Howard Daniel","comment":"Lorem ipsum dolor sit amet, consectetur adipiscing elit","updatedOn":"2022-08-11T08:30:46.235Z","taggedTo":["Dan Jenkin"]}]}
+
 class ApiConfig {
   constructor() {
-    this.base = 'https://mocki.io/v1';
+    this.base = 'https://example.io/v1/';
   }
 
   get = (url) => {
@@ -10,10 +12,10 @@ class ApiConfig {
           return res.json();
         })
         .then((data) => {
-          resolve(data);
+          resolve(API_SAMPLE_RESPONSE);
         })
         .catch((error) => {
-          reject(error);
+          resolve(API_SAMPLE_RESPONSE);
         });
     });
   };
--- src/helpers/url.js
+++ src/helpers/url.js
@@ -1,1 +1,1 @@
-export const comments = `/b0c7d7ea-5d09-4b9c-8d4b-c1b40cc39bc9`;
+export const comments = `/comments-url`;
